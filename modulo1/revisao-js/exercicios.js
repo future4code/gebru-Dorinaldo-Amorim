0d9e0c7fc812e8220fefdd28f94eedd51938789a
477885555 <dorinaldosa@gmail.com>
2021-12-12 22:53:12 -0300

    atualisando-revisao-js

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -5,45 +5,117 @@
 
 // EXERCÍCIO 01
 function retornaTamanhoArray(array) {
-   
 
-    
+   return array.length
+
+   
 }
 
 // EXERCÍCIO 02
 function retornaArrayInvertido(array) {
-  
+
+    return array.reverse()
 }
 
 // EXERCÍCIO 03
 function retornaArrayOrdenado(array) {
-  
+
+    return array.sort((a,b) => a-b)
+ 
 }
 
 // EXERCÍCIO 04
 function retornaNumerosPares(array) {
-  
+
+         const numerosPares = array.filter(
+
+                            function funcaoArray (numero) {
+                               return numero % 2 === 0
+                              }
+    
+        )
+return numerosPares
+
 }
+
 
 // EXERCÍCIO 05
 function retornaNumerosParesElevadosADois(array) {
+    
+
+            newarray = []
+
+        for (let num of array)          
+           if (num % 2 === 0) {
+             newarray.push(num * num) 
+        
+     }
+return newarray
  
 }
 
 // EXERCÍCIO 06
 function retornaMaiorNumero(array) {
   
+  
+    let maior = 0
+
+    for (let num of array)          
+       if (num > maior) {
+           maior = num    
+    
+    }
+return maior
+
 }
 
 // EXERCÍCIO 07
 function retornaObjetoEntreDoisNumeros(num1, num2) {
 
+  
+    if (num1 >= num2) {
+
+        let maiorNumero1 = num1
+        let maiorDivisivelPorMenor1 = num1 % num2 === 0
+        let diferenca1 =num1 - num2
+
+        const objeto = {
+            maiorNumero : maiorNumero1,
+            maiorDivisivelPorMenor: maiorDivisivelPorMenor1,
+            diferenca: diferenca1
+        } 
+        return objeto
+   
+    }  else if (num1 < num2)  {
+        let maiorNumero1 = num2
+        let maiorDivisivelPorMenor1 = num2 % num1 === 0
+        let diferenca1 =num2 - num1
+
+        const objeto = {
+            maiorNumero : maiorNumero1,
+            maiorDivisivelPorMenor: maiorDivisivelPorMenor1,
+            diferenca: diferenca1
+        } 
+        return objeto
+
+    } 
+    return objeto
+
 }
 
 // EXERCÍCIO 08
 function retornaNPrimeirosPares(n) {
+
+    
+    Pares = []
+
+    if ( n % 2 === 0 ) {
+         Pares.push(n) 
+    
+ }
+return Pares
    
-}
+} 
 
 // EXERCÍCIO 09
 function classificaTriangulo(ladoA, ladoB, ladoC) {
