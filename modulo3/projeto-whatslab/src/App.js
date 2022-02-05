1d579f8e9b0dc91c30555ea0df1674d954dea784
477885555 <dorinaldosa@gmail.com>
2022-02-04 23:47:40 -0300

    whatzap-Labenu

---
+++ modulo3/projeto-whatslab/src/App.js
@@ -0,0 +1,30 @@
+
+import styled from 'styled-components';
+import React from 'react';
+ 
+export default class App extends  React.Component{
+
+
+  render () {
+    return (
+      <div>
+
+
+
+
+
+
+
+
+
+
+
+
+      </div>
+ 
+      )
+    }
+  }
+
+
+ 
