97df14fa3ce398c4c0b6375a342a0bd1e030d6b0
477885555 <dorinaldosa@gmail.com>
2022-02-05 01:13:04 -0300

    projeto-quinzenal-projetowhats

---
+++ modulo3/projetowhats/src/App.js
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+import React from "react";
+
+/**----------------------------------------------------------------------- */
+
+const conteiner = styled.div`
+ display: flex;
+`
+
+
+
+export default class App extends React.Component {
+
+
+  render () {
+    return (
+      <div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      </div>
+    )
+  }
+ 
+}
+
+
