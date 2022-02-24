b187770981b8898e9fb86e8ef8233f2eea96cf34
477885555 <dorinaldosa@gmail.com>
2022-02-24 16:09:19 -0300

    apis-labenusers

---
+++ modulo3/apis-labenusers/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+import './App.css';
+
+
+const Div = styled.div`
+text-align: center;
+`
+
+function App() {
+  return (
+    <Div >
+   <p>olá mundo</p>
+    </Div>
+  );
+}
+
+export default App;
