677d75e8865771e701f4bf720290f9f11afbe02a
477885555 <dorinaldosa@gmail.com>
2022-04-13 05:16:34 -0300

    eparando-route

--- modulo4/projeto-labex/src/App.js
+++ modulo4/projeto-labex/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import GlobalStyle from "./globalStyles";
-import { Router } from "./routes/Router";
+import Router from "./routes/Router";
 
 const App = () => {
   return (
