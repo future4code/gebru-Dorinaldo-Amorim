1e25ebdbc043949afce4317c44135ab6e6381cd1
477885555 <dorinaldosa@gmail.com>
2022-05-19 06:19:23 -0300

    exercicio-node=json

---
+++ Back-end/node-package-json/Exercicio 01/index.js
@@ -0,0 +1,12 @@
+// exer 1
+// a) para acessar os paranmetros usa-se process.argv[]
+
+
+
+const idade = Number(process.argv[3])
+
+console.log(`Olá, ${process.argv[2]}! Você tem ${idade} anos`)
+console.log(`Olá, ${process.argv[2]}! Você tem ${idade} anos. Em sete anos você terá ${idade + 7}`)
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////
---
+++ Back-end/node-package-json/Exercicio 03/index.js
@@ -0,0 +1,19 @@
+
+
+    /////////////////////////////////////////////////////////////////////////////
+
+    // Exerc 3
+
+    // OBSEERVAÇÃO USE: npm start taref NOME.TAREFA
+    const indicadorDeAddTarefa = process.argv[2]
+    const tarefa = process.argv[3]
+    const listaDeTarefas= ['lavar a casa', "fazer o almoço"]
+
+    if (indicadorDeAddTarefa === "taref") {
+        listaDeTarefas.push(tarefa)
+        console.log(`Minhas tarefas: ${listaDeTarefas}`)
+    }
+
+/////////////////////////////////////////////////////////////////////////////////////
+//Exer 4
+
