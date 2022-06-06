d3df64bbc9ed34591f5594740c6b5dbfe147e4eb
477885555 <dorinaldosa@gmail.com>
2022-06-06 00:09:29 -0300

    finalizando-aprofundamento-express

--- Back-end/aprofundamento-express/src/index.ts
+++ Back-end/aprofundamento-express/src/index.ts
@@ -1,19 +1,104 @@
 import express from "express";
+import { ToDo, todos } from './exercicio'
 
-import { AddressInfo } from "net";
 
 const app = express();
 
 app.use(express.json());
 
-const server = app.listen(process.env.PORT || 3003, () => {
-  if (server) {
-    const address = server.address() as AddressInfo;
-    console.log(`Server is running in http://localhost:${address.port}`);
+app.get("/ping", (req, res) => {
+  res.send("Pong!")
+})
+
+//exer4
+
+app.get("/todos/completed/:isCompleted", (req, res) => {
+  let isCompleted: any = req.params.isCompleted
+
+  if (isCompleted === "true") {
+    isCompleted = true
+  } else if (isCompleted === "false") {
+    isCompleted = false
   } else {
-    console.error(`Failure upon starting server.`);
+    res.send("Path param da tarefa deve ser 'true' ou 'false'")
   }
-});;
+
+  const result = []
+
+  for (let i = 0; i < todos.length; i++) {
+    if (todos[i].completed === isCompleted) {
+      result.push(todos[i])
+    }
+  }
+
+  res.send(result)
+})
+
+//exer5
+
+app.post("/todos", (req, res) => {
+  const userId = req.body.userId
+  const title = req.body.title
+
+  const newTodo: ToDo = {
+    userId,
+    id: Date.now(),
+    title,
+    completed: false
+  }
+
+  todos.push(newTodo)
+
+  res.send(todos)
+})
+
+//exer6
+
+app.put("/todos/:id/completed", (req, res) => {
+
+  const id = Number(req.params.id)
+
+  for (let todo of todos) {
+    if (todo.id === id) {
+      todo.completed = !todo.completed
+    }
+  }
+
+  res.send(todos)
+})
+
+//exer7
+
+app.delete("/todos/:id", (req, res) => {
+
+  const id = Number(req.params.id)
+
+  for (let i = 0; i < todos.length; i++) {
+    if (todos[i].id === id) {
+      todos.splice(i, 1)
+    }
+  }
+
+  res.send(todos)
+})
+
+//exer8
+
+app.get("/users/:userId/todos", (req, res) => {
+  const userId = Number(req.params.userId)
+
+  const result = []
+
+  for (let todo of todos) {
+    if (todo.userId === userId) {
+      result.push(todo)
+    }
+  }
+
+  res.send(result)
+})
+
+app.listen(3003, () => console.log("Servidor disponível em 3003"))
 
 
 
