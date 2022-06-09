98b77cbbbd482f3fb25d1335c7c6ab753500aaed
477885555 <dorinaldosa@gmail.com>
2022-06-09 15:52:59 -0300

    apis-rest

---
+++ Back-end/apis-rest/src/index.ts
@@ -0,0 +1,20 @@
+import cors from 'cors'
+import express from "express";
+import {users} from './data'
+import { AddressInfo } from "net";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+
+
+const server = app.listen(process.env.PORT || 3003, () => {
+  if (server) {
+    const address = server.address() as AddressInfo;
+    console.log(`Server is running in http://localhost:${address.port}`);
+  } else {
+    console.error(`Failure upon starting server.`);
+  }
+});;
